86a7687116fa4a0e21062817f1abd94a571698a7
steven <hhuang@ban-qu.com>
2024-10-17 21:24:18 -0400

    /- unused import

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { AccordionComponent } from "@/components/homepage/accordion-component";
 import BlogSample from "@/components/homepage/blog-samples";
-import HeroSection from "@/components/homepage/hero-section";
 import MarketingCards from "@/components/homepage/marketing-cards";
 import Pricing from "@/components/homepage/pricing";
 import SideBySide from "@/components/homepage/side-by-side";
